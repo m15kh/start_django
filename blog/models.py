4f1a2bd0f1c3628693c2ca0465d84c2073602dc3
Mohammad <91499974+m15kh@users.noreply.github.com>
2023-07-20 14:17:20 +0330

    conncet database

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField()
+    # author = 
+    # img = 
+    title = models.CharField(max_length=256)
     content = models.TextField()
+    # category
+    # tag
+    counted_view = models.IntegerField()#default=0
+    status = models.BooleanField()
+        # create_date
+
+    # published_date
+    # uploaded_date
+
